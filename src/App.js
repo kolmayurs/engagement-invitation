971c7e88169d5c97d5274337b767e06e77f11c99
Mayur Koli <kolimayurs@gmail.com>
2018-05-09 17:18:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,63 @@ import halfenv from './img/halenv.png';
 import rings from './img/rings.png'
 
 class App extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = { imageStatus: "loading", imgLoading:true, imgError: false, render: false };
+  }
+
+  handleImageLoaded() {
+    this.setState({ imageStatus: "loaded",  imgLoading:false, imgError: false });
+  }
+
+  handleImageErrored() {
+    this.setState({ imageStatus: "failed to load",  imgLoading:false, imgError: true });
+  }
+  componentDidMount(){
+    setTimeout(function() { //Start the timer
+      this.setState({imageStatus: "loaded",  imgLoading:false, imgError: false, render: true}) //After 1 second, set render to true
+  }.bind(this), 1000)
+  }
   render() {
-    return (
+     if(this.state.imgError){
+        alert(this.state.imgError);
+    }
+    if(this.state.imgLoading){
+      return(
+          <div className="container">
+            <div className="item item-1"></div>
+            <div className="item item-2"></div>
+            <div className="item item-3"></div>
+            <div className="item item-4"></div>
+          </div>
+        )
+
+    }
+    else{
+      return (
       <div className="main-container">
         <div className="env">
-          <img src={env} alt="Envelop" />
+          <img src={env} alt="Envelop" onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)} />
         </div>
         <div className="half-env">
-          <img src={halfenv} alt="Envelop" />
+          <img src={halfenv} alt="Envelop" onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)} />
         </div>
         <div className="invitation-box">
           <div className="box-1">
-            <img src={rings} alt="Ring" />
+            <img src={rings} alt="Ring" onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)} />
           </div>
           <div className="box-2">
-            <img src={rings} alt="Ring" />
+            <img src={rings} alt="Ring" onLoad={this.handleImageLoaded.bind(this)}
+          onError={this.handleImageErrored.bind(this)} />
           </div>
         </div>
       </div>
     );
+    }
+    
   }
 }
 
